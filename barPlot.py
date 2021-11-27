6c77cd5dcaacf6f22db24e3cae7d3001a281dde0
imkrish513 <70813672+imkrish513@users.noreply.github.com>
2021-11-27 14:44:46 -0500

    Add files via upload

---
+++ barPlot.py
@@ -0,0 +1,15 @@
+# import pandas as pd
+
+# import plotly.express as px
+
+# #reading data from csv files
+# df = pd.read_csv("data.csv")
+# fig = px.bar(df, x='Country', y='InternetUsers')
+# fig.show()
+
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("data.csv")
+fig = px.bar(df, x = 'Country', y= 'IntertUsers')
+fig.show()
---
+++ scatterplot.py
@@ -0,0 +1,17 @@
+# import pandas as pd
+# import plotly.express as px
+
+# df = pd.read_csv("data.csv")
+# fig = px.scatter(df, x="Population", y="Per capita",
+# 	          size="Percentage",color="Country",
+#                    size_max=60)
+# fig.show()
+
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("data.csv")
+fig = px,scatter(df,x = "Population", y = "Per Capita",
+                size = "Percentage", color = "Country", 
+                size_max = 60)
+fig.show()
